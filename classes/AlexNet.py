f5e1708a96c6d2774f124aa1e89015618fcafe4a
Romulo Soares <romulosoares@ice.ufjf.br>
2023-12-02 14:38:09 -0300

    Fix AlexNet class

--- classes/AlexNet.py
+++ classes/AlexNet.py
@@ -15,55 +15,20 @@ class AlexNet(nn.Module):
     """
     Neural Network model proposed by AlexNet paper
     """
-    # def __init__(self, num_classes=10):
-    #     """
-    #     Define the layers os AlexNet
-
-    #     Args:
-    #         num_classes (int, optional): number of classes to predict with this model. Defaults to 10.
-    #     """
-    #     super().__init__()
-    #     self.net = nn.Sequential(
-    #         nn.Conv2d(3, 96, kernel_size=11, stride=4),
-    #         nn.ReLU(),
-    #         nn.LocalResponseNorm(5,0.0001, beta=0.75, k=2),
-    #         nn.MaxPool2d(kernel_size=3, stride=2),
-    #         nn.Conv2d(96, 256, kernel_size=5, padding=2),
-    #         nn.ReLU(),
-    #         nn.LocalResponseNorm(5,0.0001, beta=0.75, k=2),
-    #         nn.MaxPool2d(kernel_size=3, stride=2),
-    #         nn.Conv2d(256, 384, kernel_size=3, padding=1),
-    #         nn.ReLU(),
-    #         nn.Conv2d(384, 384, kernel_size=3, padding=1),
-    #         nn.ReLU(),
-    #         nn.Conv2d(384, 256, kernel_size=3, padding=1),
-    #         nn.ReLU(),
-    #         nn.MaxPool2d(kernel_size=3, stride=2),
-    #     )
-    #     self.classifier = nn.Sequential(
-    #         nn.Dropout(p=0.5, inplace=True),
-    #         nn.Linear(256 * 6 * 6, 4096),
-    #         nn.ReLU(),
-    #         nn.Dropout(p=0.5, inplace=True),
-    #         nn.Linear(4096, 4096),
-    #         nn.ReLU(),
-    #         nn.Linear(4096, num_classes),
-    #     )
-    #     self.init_bias()
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
@@ -74,7 +39,7 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
-        # self.init_bias()
+        self.init_bias()
     
     def init_bias(self):
         for layer in self.net:
